e02be49d32b92c18b9ef1577063167a23a57ca55
Lee Boonstra <me@leeboonstra.com>
2017-03-10 16:39:47 +0100

    fix path issues

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var twitter = require('./routes/twitter');
+var twitter = require('./lib/twitter');
 
 var app = express();
 
@@ -25,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/twitter', twitter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
